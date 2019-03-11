5cb493b7f3531c86608fddd26ae78513358f4557
ZhangDi <zdmessi0404@163.com>
2019-03-11 22:14:56 +0800

    123

---
+++ src/main/java/com/messi/jenkinslearn/JenkinslearnApplication.java
@@ -0,0 +1,13 @@
+package com.messi.jenkinslearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinslearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinslearnApplication.class, args);
+	}
+
+}
